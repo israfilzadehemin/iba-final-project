3b1176fdea08a54e6242a5e79e45f282c448e566
Emin <israfilzadehemin@gmail.com>
2020-06-28 00:04:34 +0400

    -- User and Post Services partly implemented

--- src/main/java/app/service/PostService.java
+++ src/main/java/app/service/PostService.java
@@ -1,10 +1,55 @@
 package app.service;
 
+import app.entity.Post;
+import app.entity.Wishlist;
+import app.repo.PostRepo;
+import app.repo.WishlistRepo;
+import app.tool.ValidationTool;
+import javafx.geometry.Pos;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PostService {
-  public void fillAdver(String fullName, String number, String time) {
+  private final PostRepo postRepo;
+  private final WishlistRepo wishlistRepo;
+  private final ValidationTool validationTool;
 
+  public PostService(PostRepo postRepo, WishlistRepo wishlistRepo, ValidationTool validationTool) {
+    this.postRepo = postRepo;
+    this.wishlistRepo = wishlistRepo;
+    this.validationTool = validationTool;
+  }
+
+  public void fillAdver(String fullName, String number, String time) {
+//should be implemented for index
+  }
+
+  public List<Post> findAllPosts() {
+    List<Post> allPosts = postRepo.findAll();
+    if (allPosts.size() == 0) {
+      throw new NoPostEx();
+    } else {
+      return allPosts;
+    }
+  }
+
+  public List<Post> findFilteredPosts(String name, String category) {
+    if (!validationTool.isCategoryValid(category)) {
+      throw new InvalidPostCategoryEx();
+    } else {
+      List<Post> filteredPosts = postRepo.findAllbyNameAndCategory(name, Integer.parseInt(category));
+      if (filteredPosts.size() == 0) {
+        throw new NoPostEx();
+      } else {
+        return filteredPosts;
+      }
+    }
+  }
+
+  public List<Post> findWishlistPosts(String userId) {
+//should be asked from Ayshan
   }
 }
---
+++ src/main/java/app/tool/ValidationTool.java
@@ -0,0 +1,160 @@
+package app.tool;
+
+import app.entity.User;
+import app.repo.PostRepo;
+import app.repo.UserRepo;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Log4j2
+@Service
+public class ValidationTool {
+  private final UserRepo userRepo;
+  private final PostRepo postRepo;
+
+
+  public ValidationTool(UserRepo userRepo, PostRepo postRepo) {
+    this.userRepo = userRepo;
+    this.postRepo = postRepo;
+  }
+
+  public boolean isEmailUnique(String email) {
+    Optional<User> user = userRepo.findUserbyEmail(email);
+    if (user.equals(Optional.empty())) {
+      return true;
+    } else {
+      log.warn("Email is not unique: from ValidationTool.isEmailUnique() ");
+      return false;
+    }
+  }
+
+  public boolean passMatches(String pass, String passConfirm) {
+    if (pass.equals(passConfirm)) return true;
+    else {
+      log.warn("Password and Confirm password is not same: from ValidationTool.passmatches()");
+      return false;
+    }
+  }
+
+  public boolean isUsernameUnique(String username) {
+    Optional<User> user = userRepo.findUserbyUsername(username);
+    if (user.equals(Optional.empty())) {
+      return true;
+    } else {
+      log.warn("Email is not unique: from ValidationTool.isUsernameUnique() ");
+      return false;
+    }
+  }
+
+  public boolean isPhoneValid(String number) {
+    if (!isAzercell(number) && !isBakcell(number) && !isNar(number) && !isHome(number)) {
+      log.warn("Phone number is not valid: from ValidationTool.isPhoneValid()");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public boolean isAzercell(String number) {
+    try {
+      long parsedNumber = Long.parseLong(number);
+      if ((parsedNumber > 502000000 && parsedNumber < 509999999)
+              || (parsedNumber > 512000000 && parsedNumber < 519999999)) {
+        log.info("Azercell number registered");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (NumberFormatException e) {
+      log.warn("Number could not be parsed: from ValidationTool.isAzercell()");
+      return false;
+    }
+  }
+
+  public boolean isBakcell(String number) {
+    try {
+      long parsedNumber = Long.parseLong(number);
+      if ((parsedNumber > 552000000 && parsedNumber < 559999999)
+              || (parsedNumber > 992000000 && parsedNumber < 999999999)) {
+        log.info("Bakcell number registered");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (NumberFormatException e) {
+      log.warn("Number could not be parsed: from ValidationTool.isBakcell()");
+      return false;
+    }
+  }
+
+  public boolean isNar(String number) {
+    try {
+      long parsedNumber = Long.parseLong(number);
+      if ((parsedNumber > 702000000 && parsedNumber < 709999999)
+              || (parsedNumber > 772000000 && parsedNumber < 779999999)) {
+        log.info("Nar number registered");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (NumberFormatException e) {
+      log.warn("Number could not be parsed: from ValidationTool.isNar()");
+      return false;
+    }
+  }
+
+  public boolean isHome(String number) {
+    try {
+      long parsedNumber = Long.parseLong(number);
+      if (parsedNumber > 122000000 && parsedNumber < 129999999) {
+        log.info("Home number registered");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (NumberFormatException e) {
+      log.warn("Number could not be parsed: from ValidationTool.isHome()");
+      return false;
+    }
+  }
+
+  public boolean isLoginCorrect(String login, String pass) {
+    Optional<User> userbyEmail = userRepo.findUserbyEmail(login);
+    Optional<User> userByUsername = userRepo.findUserbyUsername(login);
+
+    if (!userbyEmail.equals(Optional.empty())) {
+      if (userbyEmail.get().getPassword().equals(pass)) {
+        return true;
+      } else {
+        log.warn("Password is not correct: from ValidationTool.isLoginCorrect()");
+        return false;
+      }
+    } else if (!userByUsername.equals(Optional.empty())) {
+      if (userByUsername.get().getPassword().equals(pass)) {
+        return true;
+      } else {
+        log.warn("Password is not correct: from ValidationTool.isLoginCorrect()");
+        return false;
+      }
+    } else {
+      log.warn("Username and email is not found: from ValidationTool.isLoginCorrect()");
+      return false;
+    }
+  }
+
+  public boolean isCategoryValid(String categoryId) {
+    try {
+      Integer.parseInt(categoryId);
+      return true;
+    } catch (NumberFormatException e) {
+      log.warn("CategoryId could not be parsed: from ValidationTool.isCategoryValid()");
+      return false;
+    }
+  }
+
+
+}
