6b429162cd595d0a4d630afc0200971b1972af13
ljavadovaa <ljavadovaa@gmail.com>
2020-06-27 16:56:52 +0400

    -- LoginController -- UserInfoController -- IndexController

--- src/main/java/app/controller/PostViewController.java
+++ src/main/java/app/controller/PostViewController.java
@@ -1,4 +1,17 @@
 package app.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping("/post")
 public class PostViewController {
+
+  @PostMapping
+  public String handle_post() {
+    return "manage_post";
+  }
 }
