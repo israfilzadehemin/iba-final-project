b74bee287c2caea06b1b54ab6dcda8b3d25c06bf
Emin <israfilzadehemin@gmail.com>
2020-07-08 16:07:09 +0400

    -- ForgottenPass partly implemented

--- src/main/java/app/controller/MailSenderController.java
+++ src/main/java/app/controller/MailSenderController.java
@@ -1,34 +1,55 @@
 package app.controller;
 
+import app.form.FormReset;
 import app.service.MailSenderService;
 import app.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.mail.MessagingException;
+import java.util.Random;
 
-@RestController
-@RequestMapping("forgotpassword")
+@Controller
+@RequestMapping("resetpass")
 public class MailSenderController {
-    private final  MailSenderService sender;
-    private final UserService service;
+  private final MailSenderService sender;
+  private final UserService service;
 
-    public MailSenderController(MailSenderService sender, UserService service) {
-        this.sender = sender;
-        this.service = service;
+  public MailSenderController(MailSenderService sender, UserService service) {
+    this.sender = sender;
+    this.service = service;
+  }
+
+  /**
+   * http://localhost:8080/resetpass/
+   */
+  @GetMapping
+  public String handle_get() {
+    return "forgot-pass";
+  }
+
+  /**
+   * http://localhost:8080/resetpass/
+   */
+  @PostMapping
+  public RedirectView handle_post(FormReset form, Model model) throws MessagingException {
+    String message = "Someone sent reset password request for your HANDY profile. If it was you, please click link below to reset your password";
+    Random r = new Random();
+
+    String link = "http://localhost:8080/resetpass/newpass/email=" + form.getEmail() + "&token=" + r.nextInt(999999999);
+    String text = String.format("%s \n %s", message, link);
+    if (service.isUserExistByEmail(form.getEmail())) {
+      sender.send(form.getEmail(), "Reset your password for HANDY profile", text);
+      model.addAttribute("sent", true);
+    } else {
+      model.addAttribute("sent", false);
     }
+    return new RedirectView("/resetpass");
+  }
 
-    /**
-     * http://localhost:8085/forgotpassword/send?mail=rza.ayshan@gmail.com
-     */
-
-    @RequestMapping("send")
-    public void sendMail(@RequestParam String mail) throws MessagingException {
-        String body =
-                "Can't remember your password for: ibatech ? Don't worry, we can help.\n" +
-                "To reset your password just click the link below and follow the instructions:\n" +
-                "http://localhost:8085/forgotpassword/changepassword";
-        if(service.isUserExistByEmail(mail))
-            sender.send(mail, "Forgot your password?", body);
-    }
+  @GetMapping("/newpass")
+  RedirectView handle_newpass()
 
 }
